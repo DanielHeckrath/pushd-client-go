8016a0af33d606c8106eefbfbf3519def6fe0a1a
Tim Schindler <tim@catalyst-zero.com>
2014-05-22 09:28:39 +0000

    set sound

--- util.go
+++ util.go
@@ -22,6 +22,7 @@ func newSubscribeDeviceRequestPayload(proto, token, lang string) io.Reader {
 	data.Set("proto", proto)
 	data.Set("token", token)
 	data.Set("lang", lang)
+	data.Set("badge", "0")
 
 	return bytes.NewBufferString(data.Encode())
 }
@@ -29,12 +30,13 @@ func newSubscribeDeviceRequestPayload(proto, token, lang string) io.Reader {
 func newNotifyPushNotificationRequestPayload(lang, msg string, data map[string]string) io.Reader {
 	d := url.Values{}
 	d.Set("msg."+lang, msg)
+	d.Set("sound", "default")
 
 	for key, value := range data {
 		d.Set("data."+key, value)
 	}
 
-	fmt.Printf("%#v\n", d.Encode())
+	fmt.Printf("pushd notification payload: %#v\n", d.Encode())
 
 	return bytes.NewBufferString(d.Encode())
 }
